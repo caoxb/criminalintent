3bcc76800be50659a132e8255ab0e4863909c9f2
caoxiabo <caoxb5018@126.com>
2018-06-02 22:57:04 +0800

    增加RecyclerView列表fragment

--- src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -1,16 +1,15 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.bignerdranch.android.criminalintent.fragment.CrimeFragment;
+import com.bignerdranch.android.criminalintent.fragment.CrimeListFragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
