8397a4324317d98dbca19188f28a9d8de8cfbe5f
caoxiabo <caoxb5018@126.com>
2018-06-06 07:07:26 +0800

    采用MENU手动添加罪犯记录

--- src/main/java/com/bignerdranch/android/criminalintent/data/CrimeLab.java
+++ src/main/java/com/bignerdranch/android/criminalintent/data/CrimeLab.java
@@ -23,14 +23,17 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
+        //数据来源的提供发生变化
+/*        for (int i = 0; i < 100; i++) {
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
             crime.setSolved(i % 2 == 0);
             mCrimes.add(crime);
-        }
+        }*/
     }
-
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
+    }
     public List<Crime> getCrimes() {
         return mCrimes;
     }
